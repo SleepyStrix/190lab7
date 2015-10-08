e412632bd6cc2081bba940fda852855b9ab5ffd6
Dan Hewlett <dhewlett@purdue.edu>
2015-10-08 08:53:19 -0400

    fixed tab error

--- calc.py
+++ calc.py
@@ -79,7 +79,7 @@ while (True):
             print "Product: ", mult(a, b)
         elif (op == "/"):
             print "Quotient: ", div(a, b)
-		elif (op == "**"):
+        elif (op == "**"):
 			print "Power: ", exp(a, b)
         elif (op == "%"):
             print "Mod: ", mod(a, b)
